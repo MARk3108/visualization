35addd11ec7806ebae38cca2bba07c670edfce0b
Mark <m.fomin@otk.group>
2026-06-01 10:17:40 -0400

    visualization dashboards for bank analytics

---
+++ superset_config.py
@@ -0,0 +1,16 @@
+from flask_appbuilder.security.manager import AUTH_DB
+
+SECRET_KEY = "super_secret_key_change_in_prod"
+AUTH_TYPE = AUTH_DB
+WTF_CSRF_ENABLED = False
+
+FEATURE_FLAGS = {
+    "ENABLE_TEMPLATE_PROCESSING": True,
+    "DASHBOARD_NATIVE_FILTERS": True,
+    "DASHBOARD_CROSS_FILTERS": True,
+}
+
+CACHE_CONFIG = {
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+}
